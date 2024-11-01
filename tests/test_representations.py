4dded237e5455de01c7ec39ce96328a3b4091ad1
Senwen Deng <9957-senwen.deng@users.noreply.gitlab.in2p3.fr>
2024-11-01 09:51:41 +0100

    test: adapt for phasor

--- tests/test_representations.py
+++ tests/test_representations.py
@@ -73,7 +73,7 @@ class TestSeries(unittest.TestCase):
         self.assertTrue(np.array_equal(result.entries, np.conj(self.freq_entries)))
 
         result = self.freq_series.angle()
-        self.assertTrue(np.array_equal(result.entries, np.angle(self.freq_entries)))
+        self.assertTrue(np.array_equal(result.entries, np.unwrap(np.angle(self.freq_entries))))
 
         result = self.freq_series.abs()
         self.assertTrue(np.array_equal(result.entries, np.abs(self.freq_entries)))
@@ -102,9 +102,9 @@ class TestPhasor(unittest.TestCase):
 
     def setUp(self):
         self.frequencies = np.array([1.0, 2.0, 3.0])
-        self.amplitudes = np.array([1.0, 0.5, 0.2])
+        self.amplitudes = np.arange(1, 4) * np.exp(1j * np.pi/4)
         self.phases = np.array([0.0, np.pi/4, np.pi/2])
-        self.phasor_sequence = Phasor.make(self.frequencies, self.amplitudes, self.phases)
+        self.phasor = Phasor.make(self.frequencies, self.amplitudes, self.phases)
 
     def test_reim_to_cplx(self):
         real_parts = np.array([1.0, 0.0, -1.0])
@@ -113,13 +113,13 @@ class TestPhasor(unittest.TestCase):
         result = Phasor.reim_to_cplx(real_parts, imag_parts)
         assert_array_almost_equal(result, expected)
 
-    def test_cplx_to_phasor(self):
-        complex_numbers = np.array([1.0 + 0.0j, 0.0 + 1.0j, -1.0 + 0.0j])
-        expected_amplitudes = np.array([1.0, 1.0, 1.0])
-        expected_phases = np.array([0.0, np.pi/2, np.pi])
-        amplitudes, phases = Phasor.cplx_to_phasor(complex_numbers)
-        assert_array_almost_equal(amplitudes, expected_amplitudes)
-        assert_array_almost_equal(phases, expected_phases)
+    def test_cplx_to_reim(self):
+        cplx = np.array([1.0 + 0.0j, 0.0 + 1.0j, -1.0 + 0.0j])
+        expected_real_parts = np.array([1.0, 0.0, -1.0])
+        expected_imag_parts = np.array([0.0, 1.0, 0.0])
+        result_real_parts, result_imag_parts = Phasor.cplx_to_reim(cplx)
+        assert_array_almost_equal(result_real_parts, expected_real_parts)
+        assert_array_almost_equal(result_imag_parts, expected_imag_parts)
 
     def test_phasor_to_cplx(self):
         amplitudes = np.array([1.0, 1.0, 1.0])
@@ -128,29 +128,20 @@ class TestPhasor(unittest.TestCase):
         result = Phasor.phasor_to_cplx(amplitudes, phases)
         assert_array_almost_equal(result, expected)
 
-    def test_reim_to_phasor(self):
-        real_parts = np.array([1.0, 0.0, -1.0])
-        imag_parts = np.array([0.0, 1.0, 0.0])
-        expected_amplitudes = np.array([1.0, 1.0, 1.0])
-        expected_phases = np.array([0.0, np.pi/2, np.pi])
-        amplitudes, phases = Phasor.reim_to_phasor(real_parts, imag_parts)
-        assert_array_almost_equal(amplitudes, expected_amplitudes)
-        assert_array_almost_equal(phases, expected_phases)
-
     def test_get_interpolated(self):
         def dummy_interpolator(x, y):
             return lambda z: np.interp(z, x, y)
 
         new_frequencies = np.array([1.5, 2.5])
-        interpolated_sequence = self.phasor_sequence.get_interpolated(new_frequencies, dummy_interpolator)
-        expected_amplitudes = np.array([0.75, 0.35])
+        interpolated_sequence = self.phasor.get_interpolated(new_frequencies, dummy_interpolator)
+        expected_amplitudes = np.array([1.5, 2.5]) * np.exp(1j * np.pi/4)
         expected_phases = np.array([np.pi/8, 3*np.pi/8])
         assert_array_almost_equal(interpolated_sequence.amplitudes, expected_amplitudes)
         assert_array_almost_equal(interpolated_sequence.phases, expected_phases)
 
     def test_to_freq_series(self):
-        freq_series = self.phasor_sequence.to_freq_series()
-        expected_entries = np.array([1.0 + 0.0j, 0.5 * np.exp(1j * np.pi/4), 0.2 * np.exp(1j * np.pi/2)])
+        freq_series = self.phasor.to_frequency_series()
+        expected_entries = np.array([np.exp(1j * np.pi/4), 2 * np.exp(1j * np.pi/2), 3 * np.exp(3j * np.pi/4)])
         assert_array_almost_equal(freq_series.entries, expected_entries)
 
 if __name__ == '__main__':
