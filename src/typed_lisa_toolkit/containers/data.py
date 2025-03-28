snwnde/typed-lisa-toolkit

commit 869498e4000dc2fb99beaf9d4cd33581ef22158f

fix: rfft, irfft, stfft dimension

src/typed_lisa_toolkit/containers/representations.py
@@ -366,9 +366,10 @@ class FrequencySeries(
             if tapering is not None
             else np.ones_like(self.frequencies)
         )
+        dt = time_grid[1] - time_grid[0]
         return TimeSeries(
             grid=time_grid,
-            entries=np.fft.irfft(self.entries * tapering_window, n=len(time_grid)),
+            entries=np.fft.irfft(self.entries * tapering_window / dt, n=len(time_grid)),
         )
 
     def get_time_shifted(self, shift: float):
@@ -407,7 +408,7 @@ class TimeSeries(
         )
         return FrequencySeries(
             grid=np.fft.rfftfreq(n=len(self.times), d=self.dt),
-            entries=np.fft.rfft(self.entries * tapering_window),
+            entries=np.fft.rfft(self.entries * tapering_window * self.dt),
         )
 
     def get_plotter(self) -> plotters.TSPlotter:
@@ -421,7 +422,7 @@ class TimeSeries(
         SFT = scipy.signal.ShortTimeFFT(win=win, hop=hop, fs=1 / self.dt)
         times = SFT.t(len(self.entries)) + self.times[0]
         freqs = SFT.f
-        Sx = SFT.stft(self.entries)
+        Sx = SFT.stft(self.entries * self.dt)
         return TimeFrequency(times=times, frequencies=freqs, entries=Sx)
 
 

commit 638b3e0d1526d7950257acefeefd608db33bcdec

fix: fft convention
The commit 869498e (fix: rfft, irfft, stfft dimension, 2025-03-19) introduced an error of repetition for the Data classes' transformation. This patch fixes it.

src/typed_lisa_toolkit/containers/data.py
@@ -51,7 +51,6 @@ from collections.abc import Mapping
 import logging
 import pathlib
 from typing import TypeVar, Generic, Self, TYPE_CHECKING, Literal, overload
-import warnings
 
 import numpy as np
 import numpy.typing as npt
@@ -252,10 +251,7 @@ class TSData(_SeriesData[representations.TimeSeries[NPFloatingT, NPFloatingTb]])
         :class:`.FSData` | :class:`.TimedFSData`
             The frequency series data. If `keep_times` is `True`, the time grid is kept.
         """
-        fsdict = {
-            chnname: (chn * self.dt).rfft(tapering=tapering)
-            for chnname, chn in self.items()
-        }
+        fsdict = {chnname: chn.rfft(tapering=tapering) for chnname, chn in self.items()}
         if keep_times:
             return TimedFSData(fsdict, times=self.times)
         return FSData(fsdict)
@@ -370,12 +366,8 @@ class FSData(_SeriesData[representations.FrequencySeries[NPFloatingT, NPNumberT]
         tapering: representations.Tapering | None,
     ):
         """Return the time series data."""
-        dt: NPFloatingT = times[1] - times[0]
-        nyquist_dt = 1 / (2 * self.frequencies[-1])
-        if dt < nyquist_dt and not np.isclose(dt, nyquist_dt):
-            warnings.warn("The time grid is denser than the Nyquist limit.")
         tsdict = {
-            chnname: (chn / dt).irfft(times, tapering=tapering)
+            chnname: chn.irfft(times, tapering=tapering)
             for chnname, chn in self.items()
         }
         return TSData(tsdict)

src/typed_lisa_toolkit/containers/representations.py
@@ -83,6 +83,7 @@ from collections.abc import Callable
 import dataclasses as dc
 import logging
 from typing import TypeVar, Generic, Self, Protocol, TYPE_CHECKING
+import warnings
 
 import numpy as np
 import numpy.typing as npt
@@ -366,7 +367,10 @@ class FrequencySeries(
             if tapering is not None
             else np.ones_like(self.frequencies)
         )
-        dt = time_grid[1] - time_grid[0]
+        dt: np.floating = time_grid[1] - time_grid[0]
+        nyquist_dt = 1 / (2 * self.frequencies[-1])
+        if dt < nyquist_dt and not np.isclose(dt, nyquist_dt):
+            warnings.warn("The time grid is denser than the Nyquist limit.")
         return TimeSeries(
             grid=time_grid,
             entries=np.fft.irfft(self.entries * tapering_window / dt, n=len(time_grid)),