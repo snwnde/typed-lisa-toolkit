8814ba2aa9132ef18d4eb80730ac32c4f9feb005
Senwen Deng <36327760+snwnde@users.noreply.github.com>
2026-06-08 20:55:32 +0200

    fix: plot compare showdiff

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -9,8 +9,12 @@ def test_plot_tsdata(tsdata: tlt.types.TSData):
 def test_plot_compare_tsdata(tsdata: tlt.types.TSData):
     _ = tlt.plot_compare(tsdata.set_name("Test 1"), tsdata.set_name("Test 2"))
     _ = tlt.plot_compare(tsdata.pick("X"), tsdata.pick("X"), set_legend=False)
+    _ = tlt.plot_compare(
+        tsdata.pick("X"), tsdata.pick("X"), set_legend=False, showdiff=True
+    )
 
 
 def test_plot_time_series(tsdata: tlt.types.TSData):
     _ = tlt.plot(tsdata["X"], set_legend=False)
     _ = tlt.plot_compare(tsdata["X"], tsdata["Y"], set_legend=False)
+    _ = tlt.plot_compare(tsdata["X"], tsdata["Y"], set_legend=False, showdiff=True)
