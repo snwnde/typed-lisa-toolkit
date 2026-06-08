47fe867f64f818e49ee91bbd3edee02ea1d75f89
Senwen Deng <senwen.deng@l2it.in2p3.fr>
2026-06-08 17:10:25 +0200

    fix: plot

---
+++ tests/test_plot.py
@@ -0,0 +1,16 @@
+import typed_lisa_toolkit as tlt
+
+
+def test_plot_tsdata(tsdata: tlt.types.TSData):
+    _ = tlt.plot(tsdata, set_legend=False)
+    _ = tlt.plot(tsdata.pick("X"), set_legend=False)
+
+
+def test_plot_compare_tsdata(tsdata: tlt.types.TSData):
+    _ = tlt.plot_compare(tsdata, tsdata, set_legend=False)
+    _ = tlt.plot_compare(tsdata.pick("X"), tsdata.pick("X"), set_legend=False)
+
+
+def test_plot_time_series(tsdata: tlt.types.TSData):
+    _ = tlt.plot(tsdata["X"], set_legend=False)
+    _ = tlt.plot_compare(tsdata["X"], tsdata["Y"], set_legend=False)
