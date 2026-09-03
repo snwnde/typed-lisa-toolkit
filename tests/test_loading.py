a585171f01a66b69ec58303773b64b237f87976b
Senwen Deng <senwen.deng@l2it.in2p3.fr>
2026-09-03 11:17:22 +0200

    ci: added cache

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -5,8 +5,7 @@ import pytest
 import typed_lisa_toolkit as tlt
 
 
-# Keep this mark until caching is configured in CI
-@pytest.mark.slow("requires downloading data")
+@pytest.mark.slow("requires downloading non reduced data")
 def test_load_mojito():
     combined = mojito.download.download_brick("combined", reduced=False)
     # Reduced data has no quality flag so we must use the non-reduced version.
@@ -14,14 +13,13 @@ def test_load_mojito():
     assert data.channel_names == ("X", "Y", "Z", "flag")
 
 
-@pytest.mark.slow("requires downloading data")
 def test_load_reduced_mojito():
     combined = mojito.download.download_brick("combined", reduced=True)
     data = tlt.load_mojito(combined, time_interval=(None, tlt.shop.month2second(6)))
     assert data.channel_names == ("X", "Y", "Z")
 
 
-@pytest.mark.slow("requires downloading data")
+@pytest.mark.slow("requires downloading non reduced data")
 def test_load_orbits():
     orbits = tlt.load_mojito_orbits(
         mojito.download.download_brick("combined", reduced=False)
@@ -29,7 +27,6 @@ def test_load_orbits():
     assert isinstance(orbits, lisaorbits.Orbits)
 
 
-@pytest.mark.slow("requires downloading data")
 def test_load_reduced_orbits():
     orbits = tlt.load_mojito_orbits(
         mojito.download.download_brick("combined", reduced=True)
